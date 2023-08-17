0c335ce81d2d7837d0a530790c472788a7658a22
Christian Danielsen <ckdanielsen@gmail.com>
2023-08-16 23:33:36 -0700

    feat: add prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App.tsx'
-import './Main.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.tsx";
+import "./Main.css";
 
 // eslint-disable-next-line
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
