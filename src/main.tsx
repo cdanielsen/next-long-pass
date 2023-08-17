4e39279eb1b5a21d263aef473d8ee60419da395c
Christian Danielsen <ckdanielsen@gmail.com>
2023-08-16 23:21:31 -0700

    feat: remove some boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './components/App.tsx'
+import './Main.css'
 
+// eslint-disable-next-line
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
