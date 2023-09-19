0aa2ff25d30c393854872c08dd0c8b3b5b0d7d1d
Christian Danielsen <ckdanielsen@gmail.com>
2023-09-19 10:12:00 -0700

    feat/add-copy-button-with-icon

--- src/components/Passphrase.tsx
+++ src/components/Passphrase.tsx
@@ -1,14 +1,47 @@
-import "./Passphrase.css";
+import Typewriter from "typewriter-effect";
+import { PiCopySimpleDuotone as CopyIcon } from "react-icons/pi";
+import { Flex, IconButton } from "@chakra-ui/react";
 
 interface PassphraseProps {
   passphrase: string;
 }
 
+const copyTextToClipboard = (passphrase: string) => () => {
+  void (async () => {
+    try {
+      await navigator.clipboard.writeText(passphrase);
+    } catch (e) {
+      console.error(e);
+    }
+  })();
+};
+
 const Passphrase = ({ passphrase }: PassphraseProps) => {
   return (
-    <section id="Passphrase">
-      <code>{passphrase}</code>
-    </section>
+    <Flex
+      id="Passphrase"
+      direction="row"
+      justify="space-between"
+      align="center"
+      width="100%"
+      fontSize={"1.5rem"}
+    >
+      <Typewriter
+        options={{
+          autoStart: true,
+          delay: 40,
+          strings: passphrase,
+          cursor: "",
+        }}
+      />
+      <IconButton
+        variant="ghost"
+        colorScheme="blue"
+        aria-label="Copy passphrase"
+        icon={<CopyIcon />}
+        onClick={copyTextToClipboard(passphrase)}
+      />
+    </Flex>
   );
 };
 
